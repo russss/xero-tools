1f3255d67e0d88980e86b11a840bcc039bc28fbd
Russ Garrett <russ@garrett.co.uk>
2014-07-25 23:54:03 +0100

    Major updates:
    * Use pyxero instead of XeroPy
    * Work on GoCardless payments instead of bills
    * Deal with VAT.

--- gocardless-xero.py
+++ gocardless-xero.py
@@ -5,24 +5,27 @@ from datetime import datetime, timedelta
 from ConfigParser import ConfigParser
 
 from xero import Xero
+from xero.auth import PrivateCredentials
 import gocardless
 
 date_cutoff = datetime.now() - timedelta(days=300)
-gcid_regex = re.compile(r'\(GCID: ([0-9A-Z]+)\)')
+gcid_regex = re.compile(r'\GoCardless payout ([0-9A-Z]+)')
+
 
 class GoCardlessPaymentImporter(object):
 
     def __init__(self, config):
         self.config = config
-        self.xero = Xero(config.get('xero', 'consumer_key'),
-                        config.get('xero', 'consumer_secret'),
-                        config.get('xero', 'private_key_file'))
+        with open(config.get('xero', 'private_key_file')) as keyfile:
+            rsa_key = keyfile.read()
 
+        credentials = PrivateCredentials(config.get('xero', 'consumer_key'), rsa_key)
+        self.xero = Xero(credentials)
         gocardless.environment = config.get('gocardless', 'environment')
         gocardless.set_details(app_id=config.get('gocardless', 'app_id'),
-                                app_secret=config.get('gocardless', 'app_secret'),
-                                access_token=config.get('gocardless', 'access_token'),
-                                merchant_id=config.get('gocardless', 'merchant_id'))
+                               app_secret=config.get('gocardless', 'app_secret'),
+                               access_token=config.get('gocardless', 'access_token'),
+                               merchant_id=config.get('gocardless', 'merchant_id'))
         self.gc_client = gocardless.client
 
     def do_import(self):
@@ -33,7 +36,7 @@ class GoCardlessPaymentImporter(object):
         print "Submitted %s new journal entries" % len(to_submit)
 
     def get_posted_journals(self):
-        existing_journals = self.xero.manualjournals.filter(Since=date_cutoff, Status='POSTED')
+        existing_journals = self.xero.manualjournals.filter(since=date_cutoff, status='POSTED')
         if existing_journals is not None:
             if not isinstance(existing_journals, (list, tuple)):
                 existing_journals = [existing_journals]
@@ -43,30 +46,32 @@ class GoCardlessPaymentImporter(object):
                 if result:
                     yield result.groups(1)[0]
 
-    def parse_gocardless_bill(self, bill):
-        """ Convert a GoCardless bill object into a JSON representation of the Xero journal XML. """
+    def parse_gocardless_payout(self, payout):
+        """ Convert a GoCardless payout object into a JSON representation of the Xero journal XML. """
         return {
-            'Narration': "GoCardless payment from %s (GCID: %s)" % (bill.user().email, bill.id),
+            'Narration': "GoCardless payout %s" % payout.id,
             'Status': 'POSTED',
-            'Date': bill.paid_at.isoformat(),
+            'Date': payout.paid_at.isoformat(),
+            'LineAmountTypes': 'Inclusive',
             'JournalLines': [
-                { 'LineAmount': str(-(Decimal(bill.amount))),
-                                'AccountCode': self.config.get('xero', 'sales_account') },
-                { 'LineAmount': str(Decimal(bill.gocardless_fees)), 
-                                'AccountCode': self.config.get('xero', 'commission_account') },
-                { 'LineAmount': str(Decimal(bill.amount) - Decimal(bill.gocardless_fees)),
-                                'AccountCode': self.config.get('xero', 'gocardless_account') }
-                ]
-            }
+                {'LineAmount': str(-(Decimal(payout.amount))),
+                 'AccountCode': self.config.get('xero', 'sales_account')},
+                {'LineAmount': str(Decimal(payout.transaction_fees)),
+                 'AccountCode': self.config.get('xero', 'commission_account'),
+                 'TaxType': 'EXEMPTINPUT'},
+                {'LineAmount': str(Decimal(payout.amount) - Decimal(payout.transaction_fees)),
+                 'AccountCode': self.config.get('xero', 'gocardless_account')}
+            ]}
 
     def get_journals_to_submit(self, posted_journals):
-        for bill in self.gc_client.merchant().bills():
-            if bill.status == 'withdrawn' and bill.id not in posted_journals:
-                yield self.parse_gocardless_bill(bill)
+        for payout in self.gc_client.merchant().payouts():
+            if (payout.paid_at < datetime.now() and payout.paid_at > date_cutoff
+                    and payout.id not in posted_journals):
+                yield self.parse_gocardless_payout(payout)
 
     def submit_journals(self, to_submit):
         for start in range(0, len(to_submit), 100):
-            self.xero.manualjournals.put(to_submit[start:start+100])
+            self.xero.manualjournals.put(to_submit[start:start + 100])
 
 if __name__ == '__main__':
     config = ConfigParser()
